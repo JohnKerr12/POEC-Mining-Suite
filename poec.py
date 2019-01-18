6e20f08b4cf54b8510493a1fe826c46d529dfa8e
A Piece of Eight <36963711+JohnKerr12@users.noreply.github.com>
2019-01-18 23:27:08 +0000

    update
    Fixed blocking issue

--- poec.py
+++ poec.py
@@ -1,3 +1,4 @@
+import time, threading
 from tkinter import *
 import subprocess
 import sys
@@ -5,12 +6,16 @@ import os
 from tkinter import messagebox
 import webbrowser
 
+if os.path.isfile('value.txt'):
+    with open('value.txt','r') as x:
+        StringVar().set(x.read())
+
 def exit1():
     root.quit()
     root.destroy()
 
 def help1():
-    lines = ('POEC Mining suite', '', 'http://www.poeight.net/', '', '© Piece of Eight 2019 all rights reserved')
+    lines = ('POEC Mining suite', '', 'http://www.poeight.net/', '', '© Piece of Eight 2018 all rights reserved')
     msg = messagebox.showinfo('About', "\n".join(lines))
 
 def help2():
@@ -26,6 +31,9 @@ def hyperlink():
 def hyperlink2():
     webbrowser.open_new(r"http:209.97.185.191:3001/")
 
+def hyperlink3():
+    webbrowser.open_new(r"https://sneakytime.com/rr/")
+
 if __name__ == "__main__":
 
     root = Tk()
@@ -37,6 +45,7 @@ if __name__ == "__main__":
     def reload():
         python = sys.executable
         os.execl(python, python, * sys.argv)
+              
         
     filemenu.add_command(label="Reload", command=reload)
     filemenu.add_command(label="Exit", command=exit1)
@@ -48,9 +57,10 @@ if __name__ == "__main__":
     setup = LabelFrame(root, text = "Setup")
     setup.pack(fill = "both", expand = "yes")
 
+
+ 
     stratum = Label(setup, text="Stratum:").place(x = 30, y = 10)
     stratum1 = Entry()
-    stratum1.insert(0, "Enter the stratum address + port (E.g. 1.domain.com:port)")
     stratum1.place(x = 90, y = 29, width = 350)
 
     wallet = Label(setup, text="Wallet:").place(x = 30, y = 40)
@@ -65,25 +75,32 @@ if __name__ == "__main__":
     #extra = Entry()
     #extra.place(x = 90, y = 120, width = 350)
 
+ 
     def edit():
-        with open('poec.bat', 'wt') as f:
+        with open('poec.bat', 'wt') as f, open('values.txt', 'wt') as v:
             if not stratum1.get():
                 msg = messagebox.showerror("Error", "Please fill out the stratum box, including the port number.")
             else:
                 f.write("ccminer-x64 -a scrypt -o stratum+tcp:/")
                 f.write("/" + stratum1.get())
+                v.write("ccminer-x64 -a scrypt -o stratum+tcp:/")
+                v.write("/" + stratum1.get())
             if not wallet1.get():
                 msg = messagebox.showerror("Error", "Please fill out the wallet box.")
             else:
                 f.write(" -")
                 f.write("u " + wallet1.get())
+                v.write(" -")
+                v.write("u " + wallet1.get())
             if not password1.get():
                 msg = messagebox.showerror("Error", "Please fill out the password box, type in x if not required.")
-            else:
-                    
+            else:    
                 f.write(" -")
                 f.write("p " + password1.get())
+                v.write(" -")
+                v.write("p " + password1.get())
                 f.close()
+                v.close
 
     def start():
         if not stratum1.get():
@@ -93,16 +110,29 @@ if __name__ == "__main__":
         if not password1.get():
             msg = messagebox.showerror("Error", "Please fill out the password box, type in x if not required.")
         else:
-            subprocess.call([r'poec.bat'])
+            bat()
+
+    def bat():
+        def callback():
+            root.update_idletasks()
+            bat2 = subprocess.call([r'poec.bat'])
+        t = threading.Thread(target=callback)
+        t.start()
+    root.update()
         
+        
+
     save = Button(text = "Save batch file", command = edit).place(x=120, y = 200)
     save = Button(text = "Start Miner", command = start).place(x=220, y = 200)
     help = Button(text = "Need help?", command = help2).place(x=460, y = 25)
     Poeight = Button(text = "Poeight.net", command = hyperlink).place(x=460, y = 70)
     blockexplorer = Button(text = "Block Explorer", command = hyperlink2).place(x=460, y = 120)
+    donate = Button(text = "Donate", command = hyperlink3).place(x=460, y = 170)
+
 
     def on_closing():
         if messagebox.askokcancel("Quit", "Do you really want to quit?"):
+            
             root.quit()
             root.destroy()
             sys.exit()
@@ -114,3 +144,4 @@ if __name__ == "__main__":
     root.resizable(False, False)
     root.config(menu=menubar)
     root.mainloop()
+    root.quit()
