20a77582bf3c07028c278e1300af9b44d3b6dbe2
A Piece of Eight <36963711+JohnKerr12@users.noreply.github.com>
2019-01-18 23:40:21 +0000

    Update poec.py

--- poec.py
+++ poec.py
@@ -31,21 +31,17 @@ def hyperlink():
 def hyperlink2():
     webbrowser.open_new(r"http:209.97.185.191:3001/")
 
-def hyperlink3():
-    webbrowser.open_new(r"https://sneakytime.com/rr/")
-
 if __name__ == "__main__":
 
     root = Tk()
-    root.title('POEC Mining suite v1.0')
+    root.title('POEC Mining suite v1.1')
     menubar = Menu(root)
     filemenu = Menu(menubar, tearoff=0)
     helpmenu = Menu(menubar, tearoff=0)
 
     def reload():
         python = sys.executable
-        os.execl(python, python, * sys.argv)
-              
+        os.execl(python, python, * sys.argv)         
         
     filemenu.add_command(label="Reload", command=reload)
     filemenu.add_command(label="Exit", command=exit1)
@@ -57,8 +53,6 @@ if __name__ == "__main__":
     setup = LabelFrame(root, text = "Setup")
     setup.pack(fill = "both", expand = "yes")
 
-
- 
     stratum = Label(setup, text="Stratum:").place(x = 30, y = 10)
     stratum1 = Entry()
     stratum1.place(x = 90, y = 29, width = 350)
@@ -74,8 +68,7 @@ if __name__ == "__main__":
     #extra = Label(setup, text="Params:").place(x = 30, y = 100)
     #extra = Entry()
     #extra.place(x = 90, y = 120, width = 350)
-
- 
+  
     def edit():
         with open('poec.bat', 'wt') as f, open('values.txt', 'wt') as v:
             if not stratum1.get():
@@ -119,16 +112,12 @@ if __name__ == "__main__":
         t = threading.Thread(target=callback)
         t.start()
     root.update()
-        
-        
-
+       
     save = Button(text = "Save batch file", command = edit).place(x=120, y = 200)
     save = Button(text = "Start Miner", command = start).place(x=220, y = 200)
     help = Button(text = "Need help?", command = help2).place(x=460, y = 25)
     Poeight = Button(text = "Poeight.net", command = hyperlink).place(x=460, y = 70)
     blockexplorer = Button(text = "Block Explorer", command = hyperlink2).place(x=460, y = 120)
-    donate = Button(text = "Donate", command = hyperlink3).place(x=460, y = 170)
-
 
     def on_closing():
         if messagebox.askokcancel("Quit", "Do you really want to quit?"):
